80c52f080161f432c3e0fb77745c52c38b27c6cf
Cristian David Araujo <cristian.araujo@udea.edu.co>
2024-04-17 08:09:54 -0500

    Progress in development, although problems are occurring.

---
+++ irq_polling_c/gpio_button.h
@@ -0,0 +1,87 @@
+/**
+ * \file        gpio_button.h
+ * \brief
+ * \details
+ * \author      MST_CDA
+ * \version     0.0.1
+ * \date        07/04/2024
+ * \copyright   Unlicensed
+ */
+
+#ifndef __GPIO_BUTTON_
+#define __GPIO_BUTTON_
+
+#include <stdint.h>
+#include "hardware/timer.h"
+#include "hardware/gpio.h"
+
+#include "functs.h"
+
+/**
+ * @typedef gpio_button_t 
+ *
+ * @brief Structure to manage a single button connected to a GPIO.
+ * 
+ */
+typedef struct{
+    struct {
+        uint8_t gpio_num    : 6;        // GPIO gpio_num number
+        uint8_t dzero       : 1;        // Flag for double zero
+        uint8_t nkey        : 1;        // Flag that indicates that a key was pressed
+        uint8_t dbnc        : 1;        // Flag that indicates that debouncer is active
+    }KEY;
+}gpio_button_t;
+
+/**
+ * @brief 
+ * 
+ * @param button 
+ * @param gpio_num 
+ */
+static inline void button_init(gpio_button_t *button, uint8_t gpio_num){
+    button->KEY.dzero = 0;
+    button->KEY.nkey = 0;
+    button->KEY.dbnc = 0;
+    button->KEY.gpio_num = gpio_num;
+
+    gpio_init(button->KEY.gpio_num);
+    gpio_set_dir(button->KEY.gpio_num, GPIO_IN);
+    gpio_pull_down(button->KEY.gpio_num);
+
+    gpio_set_irq_enabled_with_callback(button->KEY.gpio_num, GPIO_IRQ_EDGE_RISE, true, buttonCallback);
+}
+
+/**
+ * @brief Enable/disable the interruptions for the button.
+ * 
+ * @param button 
+ * @param enable true to enable the button interruption, false to disable.
+ */
+static inline void button_set_irq_enabled(gpio_button_t *button, bool enable){
+    gpio_set_irq_enabled(button->KEY.gpio_num, GPIO_IRQ_EDGE_RISE, enable);
+}
+
+/** 
+ * \brief This method sets to one the flag that indicates that a zero was detected on button
+ * \param button   Pointer to keypad data structure
+ */
+static inline void button_set_zflag(gpio_button_t *button){
+    button->KEY.dzero = 1;
+}
+/** 
+ * \brief This method clears the flag that indicates that a zero was detected on button
+ * \param button   Pointer to keypad data structure
+ */
+static inline void button_clr_zflag(gpio_button_t *button){
+    button->KEY.dzero = 0;
+}
+
+/** 
+ * \brief This method returns true if a first zero was detected on button
+ * \param button   Pointer to keypad data structure
+ */
+static inline bool button_is_2nd_zero(gpio_button_t *button){
+    return button->KEY.dzero;
+}
+
+#endif
---
+++ irq_polling_c/keypad.h
@@ -0,0 +1,132 @@
+/**
+ * \file        keypad.h
+ * \brief
+ * \details
+ * \author      MST_CDA
+ * \version     0.0.1
+ * \date        05/10/2023
+ * \copyright   Unlicensed
+ */
+
+#ifndef __KEYPAD_
+#define __KEYPAD_
+
+#include <stdint.h>
+#include "hardware/gpio.h"
+
+/**
+ * \typedef key_pad_t
+ * \brief Data strcuture to manage a matrix keypad of 16 keys
+ */
+typedef struct{
+    struct key{
+    uint8_t cnt     : 2;        ///< counter to generate row sequence
+    uint8_t seq     : 4;        ///< store the current sequence state
+    uint8_t ckey    : 8;        ///< captured key with position coding
+    uint8_t dkey    : 5;        ///< captured key with decimal coding
+    uint8_t rlsb    : 4;        ///< The rows LSB position for keypad rows, the four gpios for rows must be consecutives starting in position rlsb
+    uint8_t clsb    : 4;        ///< The cols LSB position for keypad cols, the four gpios for cols must be consecutives starting in position clsb
+    uint8_t en      : 1;        ///< Enable keypad processing
+    uint8_t dzero   : 1;        ///< Flag for double zero
+    uint8_t nkey    : 1;        ///< Flag that indicates that a key was pressed
+    uint8_t dbnc    : 1;        ///< Flag that indicates that debouncer is active
+    }KEY;                       ///< All key related information              
+    uint8_t history[10];        ///< The last 10 pressed keys
+}key_pad_t;
+
+/**
+ * \brief This method initializes the keypad data structure
+ * \param kpad          pointer to keypad data structure
+ * \param rlsb          LSB position of the first row GPIO
+ * \param clsb          LSB position of the first col GPIO
+ * \param dbnc_time     Time base period for keypad debouncer
+ * \param en            True if keypad start enabled
+ */ 
+void kp_init(key_pad_t *kpad, uint8_t rlsb, uint8_t clsb, bool en);
+
+/** 
+ * \brief This method decodes the postional coding of the key to its actual value in decimal
+ * \param kpad   Pointer to keypad data structure
+ */
+void kp_decode(key_pad_t *kpad);
+
+/**
+ * \brief This method captures the currently pressed key. 
+ * It should only be called when at lest one of the columns is different from zero
+ * \param kpad      pointer to keypad data structure
+ * \param cols      value of gpios masked to the gpios connected to the keypad columns
+ */
+void kp_capture(key_pad_t *kpad, uint32_t cols);
+
+/** 
+ * \brief This method updates the sequence value that drives the keypad rows. 
+ * The sequence should be updated with a frequency higher than 100Hz
+ * \param kpad   Pointer to keypad data structure
+ */
+static inline void kp_gen_seq(key_pad_t *kpad){
+    kpad->KEY.cnt = (kpad->KEY.cnt + 1)%4;
+    kpad->KEY.seq = 0x1 << kpad->KEY.cnt;
+    gpio_put_masked(0x0000000F<<kpad->KEY.rlsb,((uint32_t)kpad->KEY.seq)<<kpad->KEY.rlsb);
+}
+
+/**
+ * @brief This method enables or disables the GPIO IRQs for the keypad columns.
+ * 
+ * @param kpad 
+ * @param en 
+ */
+static inline void kp_set_irq_enabled(key_pad_t *kpad, bool en){
+    for (int i = 0; i < 4; i++){
+        gpio_set_irq_enabled(kpad->KEY.clsb + i, GPIO_IRQ_EDGE_RISE, en);
+    }
+}
+
+/** 
+ * \brief This method returns the value of the last pressed key in decimal coding
+ * \param kpad   Pointer to keypad data structure
+ */
+static inline uint8_t kp_get_key(key_pad_t *kpad){
+    kpad->KEY.nkey = false;
+    return kpad->history[0];
+}
+
+/** 
+ * \brief This method allows to access the last 10 pressed keys
+ * \param kpad   Pointer to keypad data structure
+ */
+static inline uint8_t kp_get_keyh(key_pad_t *kpad, uint8_t n){
+    return kpad->history[n%10];
+}
+
+/** 
+ * \brief This method returns the value of the key with its positional coding
+ * \param kpad   Pointer to keypad data structure
+ */
+static inline uint8_t kp_get_keyc(key_pad_t *kpad){
+    return kpad->KEY.ckey;
+}
+
+/** 
+ * \brief This method sets to one the flag that indicates that a zero was detected on keypad columns
+ * \param kpad   Pointer to keypad data structure
+ */
+static inline void kp_set_zflag(key_pad_t *kpad){
+    kpad->KEY.dzero = 1;
+}
+/** 
+ * \brief This method clears the flag that indicates that a zero was detected on keypad columns
+ * \param kpad   Pointer to keypad data structure
+ */
+static inline void kp_clr_zflag(key_pad_t *kpad){
+    kpad->KEY.dzero = 0;
+}
+
+/** 
+ * \brief This method returns true if a first zero was detected on keypad columns
+ * \param kpad   Pointer to keypad data structure
+ */
+static inline bool kp_is_2nd_zero(key_pad_t *kpad){
+    return kpad->KEY.dzero;
+}
+
+#endif // __KEYPAD_POLLING_IRQ_
